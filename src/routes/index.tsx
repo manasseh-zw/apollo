7fae55f3c64e34b02d29c1fd4e7fff880a8858d7
manasseh-zw <manassehtchanga@gmail.com>
2025-03-22 20:02:43 +0200

    feat: add brand icons, landing page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,47 @@
-import { Button } from "@heroui/react";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useRouter } from "@tanstack/react-router";
+import { useEffect } from "react";
+import { Logo } from "../components/Icons";
+import { store } from "../lib/state/store";
 
 export const Route = createFileRoute("/")({
-	component: App,
+  component: HomeComponent,
 });
 
-function App() {
-	return (
-		<div>
-			<p className="font-rubik">hello world</p>
-			<Button color="primary">
-				hello
-			</Button>
-		</div>
-	);
+function HomeComponent() {
+  const router = useRouter();
+  const { isAuthenticated, isLoading } = store.state.authState;
+  console.log(store.state.authState);
+
+  useEffect(() => {
+    if (isLoading) {
+      return;
+    } else {
+      if (isAuthenticated) {
+        router.navigate({ to: "/research" });
+      } else {
+        router.navigate({ to: "/auth/sign-in" });
+      }
+    }
+  }, [isLoading]);
+  return (
+    <div className="h-screen w-full flex items-center justify-center bg-primary">
+      <div className="relative mb-8">
+        {/* Circular loader */}
+        <div
+          className="absolute inset-0 rounded-full animate-spin border-1 border-solid border-secondary border-t-transparent"
+          style={{
+            width: "calc(100% + 15px)",
+            height: "calc(100% + 15px)",
+            left: "-10px",
+            top: "-10px",
+          }}
+        ></div>
+
+        {/* Logo with secondary background */}
+        <div className="relative rounded-full bg-primary  flex items-center justify-center">
+          <Logo className="w-64 h-64" />
+        </div>
+      </div>
+    </div>
+  );
 }
